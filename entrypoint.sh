997e6c667076715acea4c2e3bec6143895c7ad77
Ramachandran R <ramachandranr2007@gmail.com>
2024-01-16 19:31:51 +0100

    Add Static Report Option

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,6 +62,8 @@ cp -r ./${INPUT_GH_PAGES}/last-history/. ./${INPUT_ALLURE_RESULTS}/history
 echo "generating report from ${INPUT_ALLURE_RESULTS} to ${INPUT_ALLURE_REPORT} ..."
 #ls -l ${INPUT_ALLURE_RESULTS}
 allure generate --clean ${INPUT_ALLURE_RESULTS} -o ${INPUT_ALLURE_REPORT}
+
+allure generate --clean --single-file ${INPUT_ALLURE_RESULTS} -o ${INPUT_ALLURE_STATIC_REPORT}
 #echo "listing report directory ..."
 #ls -l ${INPUT_ALLURE_REPORT}
 
